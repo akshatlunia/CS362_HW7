c7b38e619364e2a8c6b287f5e2a008cacb95a058
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:47:35 -0800

    Check if number is divisible by 400
    If divisible by 400, it is a leap year

--- test_leapyears.py
+++ test_leapyears.py
@@ -3,7 +3,7 @@ import leapyears
 
 class testleapyears(unittest.TestCase):
     def test_leap(self):
-        self.assertEqual(leapyears.leapyears(400), "The year 400 is not a leap year")
+        self.assertEqual(leapyears.leapyears(400), "The year 400 is a 400-leap year")
 
 if __name__ == '__main__':
     unittest.main()
