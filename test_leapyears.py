6e91115d961e594e3269c374bd00ab420ae879c5
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:45:19 -0800

    Tests if number is divisible by 100 now
    If divisible by 100, the year is not a leap year

--- test_leapyears.py
+++ test_leapyears.py
@@ -3,7 +3,7 @@ import leapyears
 
 class testleapyears(unittest.TestCase):
     def test_leap(self):
-        self.assertEqual(leapyears.leapyears(400), "The year 400 is a leap year")
+        self.assertEqual(leapyears.leapyears(400), "The year 400 is not a leap year")
 
 if __name__ == '__main__':
     unittest.main()
