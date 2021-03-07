04f333c2a5ff892a1aeee1fa1278f3189d1260e7
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:16:12 -0800

    Added new test for Fizz
    Asks program to return an instance of "Fizz"

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -4,6 +4,7 @@ import FizzBuzz
 class testFizzBuzz(unittest.TestCase):
     def test_cube(self):
         self.assertEqual(FizzBuzz.FizzBuzz(), 100)
+        self.assertEqual(FizzBuzz.FizzBuzz(), "Fizz")
 
 if __name__ == '__main__':
     unittest.main()
