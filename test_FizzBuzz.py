3b15a3674264fb5a559f5575aa485c7c85921fcd
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:14:17 -0800

    Updated to test a more reasonable result
    Simply returning "complete" did not make a ton of sense

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -3,7 +3,7 @@ import FizzBuzz
 
 class testFizzBuzz(unittest.TestCase):
     def test_cube(self):
-        self.assertEqual(FizzBuzz.FizzBuzz(), "complete")
+        self.assertEqual(FizzBuzz.FizzBuzz(), 100)
 
 if __name__ == '__main__':
     unittest.main()
