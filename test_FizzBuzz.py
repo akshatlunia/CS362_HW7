aacf466a3c76416fdb0ca48f434afa06afafaa83
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:30:21 -0800

    Created final test for FizzBuzz module
    Checks to see if it prints FizzBuzz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -3,7 +3,7 @@ import FizzBuzz
 
 class testFizzBuzz(unittest.TestCase):
     def test_cube(self):
-        self.assertEqual(FizzBuzz.FizzBuzz(), "Buzz")
+        self.assertEqual(FizzBuzz.FizzBuzz(), "FizzBuzz")
 
 if __name__ == '__main__':
     unittest.main()
