48568b9b1f130e5c0f946af52f26bdfe745a8825
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:44:08 -0800

    Should now allow all years divisible by 4 to be leap years

--- leapyears.py
+++ leapyears.py
@@ -1,2 +1,7 @@
 def leapyears(x):
-    pass
+    if(x%4 == 0):
+        y = "The year " + str(x) + " is a leap year"
+        return(y)
+    else:
+        y = "The year " + str(x) + " is not a leap year"
+        return(y)
