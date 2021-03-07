b8c01de9ba882a1c55b7004db8edeed33b51e730
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:28:26 -0800

    Just simplified a little bit
    I hit a realization

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,7 +2,10 @@ def FizzBuzz():
     for i in range(1, 101):
         if(i % 3 == 0):
             k = "Fizz"
-            print(k," ", end='')
+            print(k, end=' ')
+        elif(i % 5 == 0):
+            k = "Buzz"
+            print(k, end=' ')
         else:
-            print(i," ", end='')
+            print(i, end=' ')
     return k
