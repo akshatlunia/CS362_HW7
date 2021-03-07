c715a2b761c7ef4bba30a14d07e7d97392b3550f
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:31:49 -0800

    Finished implementation of FizzBuzz
    Should work as intended

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,17 @@
 def FizzBuzz():
     for i in range(1, 101):
-        if(i % 3 == 0):
+        if(i % 15 == 0):
+            f = "FizzBuzz"
+            print(f, end=' ')
+            continue
+        elif(i % 3 == 0):
             k = "Fizz"
             print(k, end=' ')
+            continue
         elif(i % 5 == 0):
             k = "Buzz"
             print(k, end=' ')
+            continue
         else:
             print(i, end=' ')
-    return k
+    return f
