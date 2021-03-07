d8504e9944304c7343cfd8b93fe8e54ed2387ea9
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:48:54 -0800

    Now leapyears should pass all test statements
    It will pass all rules of leap years

--- leapyears.py
+++ leapyears.py
@@ -1,11 +1,14 @@
 def leapyears(x):
-    if(x%100 == 0):
-        y = "The year " + str(x) + " is not a leap year"
+    if(x%400 == 0):
+        y = "The year " + str(x) + " is a 400-leap year"
         return(y)
-    else:
-        if(x%4 == 0):
-            y = "The year " + str(x) + " is a leap year"
-            return(y)
-        else:
+        if(x%100 == 0):
             y = "The year " + str(x) + " is not a leap year"
             return(y)
+        else:
+            if(x%4 == 0):
+                y = "The year " + str(x) + " is a leap year"
+                return(y)
+            else:
+                y = "The year " + str(x) + " is not a leap year"
+                return(y)
