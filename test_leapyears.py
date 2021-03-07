fa7fb8f7e424b9ad2568eb4528df43ebd8c2090a
Akshat Lunia <luniaakshat@gmail.com>
2021-03-06 18:38:53 -0800

    First test for leapyears

---
+++ test_leapyears.py
@@ -0,0 +1,9 @@
+import unittest
+import leapyears
+
+class testleapyears(unittest.TestCase):
+    def test_leap(self):
+        self.assertEqual(leapyears.leapyears(400), "The year 400 is a leap year")
+
+if __name__ == '__main__':
+    unittest.main()
